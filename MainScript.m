828569f8f2f299a75247caaf8d831f057e7f05f3
Anirudh Aggarwal <64011349+anirudh075@users.noreply.github.com>
2022-06-24 22:49:14 +0530

    Update MainScript.m

--- MainScript.m
+++ MainScript.m
@@ -1,3 +1,234 @@
+%{
+Sample Run 
+
+WELCOME TO LONG TERM STOCK MARKET SIMULATOR
+
+
+**********INSTRUCTIONS**********
+The stock market simulator starts on Jan 2020. Your intial investment is the amount of money (displayed as bank ballance) you will have for the duration of the simulation.
+The stock market will give you the option to invest in Fortune 15 companies from 2000 and display the high and low price for each term.
+You can place bids to buy stocks or sell stocks in your portfolio and view company information and statistics. 
+The objective is to earn as much money as possible!!! 
+Hint:GREED IS GOOD $$$
+
+What is your name: Anirudh
+Enter intial capital (USD): 1000000
+Choose private eqauity investment period: 
+(1) Monthly 
+(2) Quarterly 
+(3) Annually
+ Selection: 3
+Choose your brokers service:
+(1) Charles Schwab (min: $0 Fees: 12%)
+(2) J.P.Morgan Wealth Management (min: $625 Fees: 6.5%)
+(3) Marcus by Goldman Sachs (min: $10,000 Fees: 2.5%)
+(4) ZOE Financial (min: $100,000 Fees: 0.25%)
+ Selection: 2
+
+
+Welocme to New York Stock Exchange		 Date:1-2000		Bank Balance:$1000000.00
+Exclusive for J.P.Morgan Wealth Management members. Minimum bidding amount is $625.00 at 6.50%.
+
+Stock											Low Price			High Price
+MICROSOFT CORPORATION (XNAS:MSFT)                   48.94				59.31
+FORD MOTOR COMPANY (XNYS:F)                         28.14				31.22
+NVIDIA CORPORATION (XNAS:NVDA)                       0.77				1.01
+THE WALT DISNEY COMPANY (XNYS:DIS)                  35.80				37.47
+ORACLE CORPORATION (XNYS:ORCL)                      24.98				31.30
+THE COCA-COLA COMPANY (XNYS:KO)                     28.72				33.44
+UNITEDHEALTH GROUP INCORPORATED (XNYS:UNH)           6.62				7.85
+VISA INC. (XNYS:V)                                  47.44				59.31
+WALMART INC. (XNYS:WMT)                             54.75				69.00
+JPMORGAN CHASE & CO. (XNYS:JPM)                     53.79				54.33
+JOHNSON & JOHNSON (XNYS:JNJ)                        43.03				48.47
+THE PROCTER & GAMBLE COMPANY (XNYS:PG)              50.14				58.73
+BERKSHIRE HATHAWAY INC. (XNYS:BRK)                  32.92				37.76
+AMAZON.COM, INC. (XNAS:AMZN)                        64.56				91.50
+
+
+Menu:
+(1) Buy
+(2) Sell
+(3) Company Data
+(4) View Portfolio
+(5) Simulator Insturuction
+(6) Next Term
+ Select your move: 1
+
+BUY STOCK
+Enter the stock you would like to buy in XNYAS form (eg. MSFT for Microsoft): K
+Please reenter a valid stock (Use XNYAS form): NVDA
+Enter your bidding price (Please be compliant with your broker): 10
+Enter the bidding volume: 1
+Telephoning your broker. Please wait.
+Bid placed.
+
+
+Menu:
+(1) Buy
+(2) Sell
+(3) Company Data
+(4) View Portfolio
+(5) Simulator Insturuction
+(6) Next Term
+ Select your move: 1
+
+BUY STOCK
+Enter the stock you would like to buy in XNYAS form (eg. MSFT for Microsoft): NVDA
+Enter your bidding price (Please be compliant with your broker): 1
+Enter the bidding volume: 10000
+Telephoning your broker. Please wait.
+Bid placed.
+
+
+Menu:
+(1) Buy
+(2) Sell
+(3) Company Data
+(4) View Portfolio
+(5) Simulator Insturuction
+(6) Next Term
+ Select your move: 3
+
+YAHOO FINANCE
+Sorry no data is available. This is the first term please wait for the another term to enable analysis.
+
+
+Menu:
+(1) Buy
+(2) Sell
+(3) Company Data
+(4) View Portfolio
+(5) Simulator Insturuction
+(6) Next Term
+ Select your move: 6
+
+NOTIFICATION FROM J.P.MORGAN WEALTH MANAGEMENT
+Your bid for NVDA at $10.00 was not succesful since the min trading amount is $625.00.
+Your bid for NVDA at $10000.00 was succesful the broker's cut was $650.00.
+Press 1 to exit: 1
+
+
+Welocme to New York Stock Exchange		 Date:1-2001		Bank Balance:$89350.00
+Exclusive for J.P.Morgan Wealth Management members. Minimum bidding amount is $625.00 at 6.50%.
+
+Stock											Low Price			High Price
+MICROSOFT CORPORATION (XNAS:MSFT)                   30.53				32.38
+FORD MOTOR COMPANY (XNYS:F)                         27.88				28.42
+NVIDIA CORPORATION (XNAS:NVDA)                       2.15				2.38
+THE WALT DISNEY COMPANY (XNYS:DIS)                  30.02				33.52
+ORACLE CORPORATION (XNYS:ORCL)                      29.12				35.00
+THE COCA-COLA COMPANY (XNYS:KO)                     29.00				31.09
+UNITEDHEALTH GROUP INCORPORATED (XNYS:UNH)          14.10				15.55
+VISA INC. (XNYS:V)                                  21.44				32.38
+WALMART INC. (XNYS:WMT)                             56.80				58.75
+JPMORGAN CHASE & CO. (XNYS:JPM)                     54.99				57.33
+JOHNSON & JOHNSON (XNYS:JNJ)                        46.56				52.34
+THE PROCTER & GAMBLE COMPANY (XNYS:PG)              35.65				39.29
+BERKSHIRE HATHAWAY INC. (XNYS:BRK)                  45.00				49.50
+AMAZON.COM, INC. (XNAS:AMZN)                        17.31				22.38
+
+
+Menu:
+(1) Buy
+(2) Sell
+(3) Company Data
+(4) View Portfolio
+(5) Simulator Insturuction
+(6) Next Term
+ Select your move: 4
+
+Stock Name 				Volume 				 Buying Price
+NVDA                     10000                       1.00
+
+
+Menu:
+(1) Buy
+(2) Sell
+(3) Company Data
+(4) View Portfolio
+(5) Simulator Insturuction
+(6) Next Term
+ Select your move: 2
+
+SELL STOCK
+Please enter the stock you would like to sell in XNYAS form (eg. MSFT for Microsoft): nvda
+Please enter the selling price: 2
+Please enter the volume you would like to sell: 5000
+Telephoning your broker. Please wait.
+Market has been notified.
+
+
+Menu:
+(1) Buy
+(2) Sell
+(3) Company Data
+(4) View Portfolio
+(5) Simulator Insturuction
+(6) Next Term
+ Select your move: 4
+
+Stock Name 				Volume 				 Buying Price
+NVDA                      5000                       1.00
+
+
+Menu:
+(1) Buy
+(2) Sell
+(3) Company Data
+(4) View Portfolio
+(5) Simulator Insturuction
+(6) Next Term
+ Select your move: 2
+
+SELL STOCK
+Please enter the stock you would like to sell in XNYAS form (eg. MSFT for Microsoft): nvda
+Please enter the selling price: 2
+Please enter the volume you would like to sell: 5000
+Telephoning your broker. Please wait.
+Market has been notified.
+
+
+Menu:
+(1) Buy
+(2) Sell
+(3) Company Data
+(4) View Portfolio
+(5) Simulator Insturuction
+(6) Next Term
+ Select your move: 3
+
+YAHOO FINANCE
+Which company would you like to anlayze in XNYAS form (eg. MSFT for Microsoft): msft
+Processing Request. Please Wait.
+
+
+MICROSOFT CORPORATION (XNAS:MSFT)
+▼	32.38 (-41.51%)
+
+GICS Sector: Technology												
+GICS Industry: Software												
+
+Perfomance												
+---------------------------------
+Today: 0.94					Monthly: 0.99
+Last Open: 21.69			Last Close: 30.31
+
+Volatility												
+---------------------------------
+ATR(14): 13.38					ATR(20%): 10.38
+ATR Inc: 6.69				 Range Today: -1.84
+
+
+Menu:
+(1) Buy
+(2) Sell
+(3) Company Data
+(4) View Portfolio
+(5) Simulator Insturuction
+(6) Next Term
+ Select your move:
+%}
 
 clear % useful to clear your workspace and use new variables
 clc %always clear command window screens before you start
